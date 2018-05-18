77bcedf35f2bf13cd2b1a33bc01b19a67f4cd56a
FloIMT <florentcheneau@outlook.com>
2018-05-18 11:49:08 +0200

    Ajoute la fonction permettant de lire dans un fichier de trajectoire

--- Trajectoire/src/Programme_principal.java
+++ Trajectoire/src/Programme_principal.java
@@ -1,6 +1,6 @@
 
-import java.io.File;
-
+import java.io.*;
+import java.util.*;
 public class Programme_principal {
 
 	
@@ -15,7 +15,101 @@ public class Programme_principal {
 	static double dist_roues = 0.1;
 	
 	static double T0 = 0.010;
+	static int k = 4;
 	
+	
+	public static double[][] lireTrajectoire(String nomFichier) {
+		
+		String commande;
+		Vector vitesses = new Vector();
+		double[][] vitesseCourante;
+		String type;
+		
+		try
+		{
+		
+		BufferedReader aLire = new BufferedReader(new FileReader(nomFichier));
+		
+		
+		do
+		{ 
+			
+		    commande = aLire.readLine();
+		    
+		    if (commande != null) {
+		    	
+		    	type = commande.substring(0, 3);
+		    	
+		    	if (type == "LIN") {
+		    		
+		    		double d = Double.parseDouble(commande.substring(4, commande.length()));
+		    		vitesseCourante = v(line(d),  d, d);
+		    		return vitesseCourante;
+		    		
+		    	
+		    	}
+		    	
+		    	if (type == "ROT") {
+		    		
+		    		
+		    		double theta = Double.parseDouble(commande.substring(4,  commande.length()));
+		    		
+		    		vitesseCourante = v_circle(circle(dist_roues/2, theta ), dist_roues/2 , theta);
+		    		return vitesseCourante;
+		    		
+		    		
+		    	}
+		    	
+		    	if(type == "CIR") {
+		    		
+		    		double theta = Double.parseDouble(commande.substring(4,  commande.length()));
+		    		double rayon;
+		    		double theta_cir;
+		    		
+		    		int i = 0;
+		    		
+		    		String r = "";
+		    		String t = "";
+		    		
+		    		while(commande.charAt(4 + i) != ',') {
+		    			
+		    			r += commande.charAt(4 + i);
+		    			i++;
+		    			
+		    		}
+		    		
+		    		t = commande.substring(4+i+1, commande.length());
+		    		
+		    		rayon = Double.parseDouble(r);
+		    		theta_cir = Double.parseDouble(t);
+		    		
+		    		vitesseCourante = v_circle(circle(rayon, theta_cir ), rayon, theta_cir);
+		    		return vitesseCourante;
+		    	}
+		    	
+		    	
+		    }
+		    	
+		    	
+		    
+		    
+		    
+		}while (commande != null);
+		
+		
+		
+	}
+		
+		
+		catch
+		(IOException e) {
+		System.out.println("Une operation sur le fichier "+nomFichier
+		+" a leve l’exception "+e)
+		;
+		}
+		
+		return null;
+	}
 	
 	static double[] derivPol(double[] Pol) { //Renvoie les coefficients de la derivée du polynôme Pol (coefficient de degré 0 à l'indice 0)
 		
@@ -87,10 +181,10 @@ public class Programme_principal {
 		return polMult;
 	}
 	
-	static double[] line(double x0, double y0, double x1, double y1, double dqm, double dq2m, double T0, int k, double vmax, double amax){ //xi et yi : coordonnees de depart et d'arrivee, dqm et dq2m : vitesse et acceleration max. des moteurs
+	static double[] line(double d){ //xi et yi : coordonnees de depart et d'arrivee, dqm et dq2m : vitesse et acceleration max. des moteurs
 		
 		
-		double d = Math.sqrt(Math.pow((x1-x0), 2) + Math.pow((y1-y0), 2));
+		
 		double Txy = d/Vmax;
 		double Tauxy = Vmax/Amax;
 		
@@ -101,13 +195,13 @@ public class Programme_principal {
 		
 		if (Txy < Tauxy) {
 			
-			Tauxy = Math.sqrt(d/vmax);
+			Tauxy = Math.sqrt(d/Vmax);
 			Txy = Tauxy;
 		}
 		
 		if (T < Tau) {
 			
-			Tau = Math.sqrt(d/dq2m);
+			Tau = Math.sqrt(d/Emax);
 			T = Tau;
 		}
 		
@@ -124,7 +218,7 @@ public class Programme_principal {
 		return param;
 	}
 	
-	static double[][] v(double[] param, double d1, double d2, double T0, double dqm) {
+	static double[][] v(double[] param, double d1, double d2) {
 		
 		double T = param[0];
 		double Tau = param[1];
@@ -169,13 +263,13 @@ public class Programme_principal {
 	}
 	
 	
-	static double[] circle(double r, double d, double theta){
+	static double[] circle(double r, double theta){
 		
-		
+		double d = dist_roues;
 		double longueur_arc = theta * r;
 		double Txy = longueur_arc/Vmax;
 		
-		double T1 = 2*Math.PI*theta*(r-d)/(r0*Wmax);
+		double T1 = 2*Math.PI*theta*(r-dist_roues)/(r0*Wmax);
 		double T2 = 2*Math.PI*theta*(r+d)/(r0*Wmax);
 		
 		double Tauxy = Vmax/Amax;
@@ -256,8 +350,6 @@ public class Programme_principal {
 			v[i][0] = (i/n0)*V1max;
 			v[i][1] = (i/n0)*V2max;
 			
-			
-			
 		}
 		
 		for (int i = n0; i < n-n0; i++){
@@ -300,9 +392,7 @@ public class Programme_principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double[] J = {1, 1, 1, 1};
-		double[] K = derivPol(J);
-		System.out.println(K[1]);
+		
 	}
 
 }
